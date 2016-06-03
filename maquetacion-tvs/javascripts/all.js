2f8b9a010c18ded51e2bd2dd719b17df2aaed095
guitarranalon <guitarranalon@gmail.com>
2016-06-03 20:30:46 +0200

    Added lightbox plugin for the photos

--- maquetacion-tvs/javascripts/all.js
+++ maquetacion-tvs/javascripts/all.js
@@ -1416,6 +1416,8 @@ $("#header").find('.row').mobileMenu("#main-nav");
 $.menuAimCall();
 
 $.scrollUp({scrollText:"Volver arriba"});
+
+$("#content").find(".gallery").featherlightGallery({galleryFadeIn:300,openSpeed:300,type:"image"});
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
--- maquetacion-tvs/javascripts/desarrollo/invocations.js
+++ maquetacion-tvs/javascripts/desarrollo/invocations.js
@@ -4,3 +4,5 @@ $("#header").find('.row').mobileMenu("#main-nav");
 $.menuAimCall();
 
 $.scrollUp({scrollText:"Volver arriba"});
+
+$("#content").find(".gallery").featherlightGallery({galleryFadeIn:300,openSpeed:300,type:"image"});
